62284f61a5314c429bfaca7a3e208d6460830f97
john doughty <jrdoughty13@gmail.com>
2021-10-14 21:37:37 -0500

    Cleaned up controls

--- Sources/systems/Controls.hx
+++ Sources/systems/Controls.hx
@@ -17,66 +17,70 @@ class Controls extends echoes.System
 		//Keyboard.get().notify(onKeyDown, onKeyUp);
 		//Mouse.get().notify(onMouseDown, onMouseUp, onMouseMove,null);
     }
-	@u public function updateG(pos:Position, p:Player, g:GamePad,k:KeyboardComp,m:MouseComp)
+	@u public function updateK(pos:Position, p:Player, k:KeyboardComp)
 	{
 
 
-		if(k.keysHeld[KeyCode.Up] || k.keysHeld[KeyCode.W] || g.buttonsPressed[GamePad.DUP])
+		if(k.keysHeld[KeyCode.Up] || k.keysHeld[KeyCode.W])
 		{
 			pos.y -= speed;
 		}
-		else if(k.keysHeld[KeyCode.Down] || k.keysHeld[KeyCode.S] || g.buttonsPressed[GamePad.DDOWN])
+		else if(k.keysHeld[KeyCode.Down] || k.keysHeld[KeyCode.S])
 		{
 			pos.y += speed;
 		}
 
-		if(k.keysHeld[KeyCode.Left] || k.keysHeld[KeyCode.A] || g.buttonsPressed[GamePad.DLEFT])
+		if(k.keysHeld[KeyCode.Left] || k.keysHeld[KeyCode.A])
 		{
 			pos.x -= speed;
 		}
-		else if(k.keysHeld[KeyCode.Right] || k.keysHeld[KeyCode.D] || g.buttonsPressed[GamePad.DRIGHT])
+		else if(k.keysHeld[KeyCode.Right] || k.keysHeld[KeyCode.D])
 		{
 			pos.x += speed;
 		}
 
 		
-		if(Mouse.isHeld(0,m))
-		{
-			var diffX = m.x - pos.x;
-			var diffY = m.y - pos.y;
-
-			if(Math.abs(diffX) > speed)
-			{
-				if(diffX < 0)
-				{
-					pos.x -= speed;
-				}
-				else
-				{
-					pos.x += speed;
-				}
-			}
-			else
-			{
-				pos.x += diffX;
-			}
-
-			if(Math.abs(diffY) > speed)
-			{
-				if(diffY < 0)
-				{
-					pos.y -= speed;
-				}
-				else
-				{
-					pos.y += speed;
-				}
-			}
-			else
-			{
-				pos.y += diffY;
-			}
-		}
 	}
 
+	@u public function updateM(pos:Position, p:Player, m:MouseComp)
+		{		
+			if(Mouse.isHeld(0,m))
+			{
+				var diffX = m.x - pos.x;
+				var diffY = m.y - pos.y;
+	
+				if(Math.abs(diffX) > speed)
+				{
+					if(diffX < 0)
+					{
+						pos.x -= speed;
+					}
+					else
+					{
+						pos.x += speed;
+					}
+				}
+				else
+				{
+					pos.x += diffX;
+				}
+	
+				if(Math.abs(diffY) > speed)
+				{
+					if(diffY < 0)
+					{
+						pos.y -= speed;
+					}
+					else
+					{
+						pos.y += speed;
+					}
+				}
+				else
+				{
+					pos.y += diffY;
+				}
+			}
+		}
+	
 }
